37b4d8906eb12120a8a3b0fafaa019fd5c06c032
Jason Kao <jasonkao85@gmail.com>
2018-06-14 07:46:10 -0400

    THIS TOOK 7 HOURS I HATE MYSELF

--- raytracer.py
+++ raytracer.py
@@ -55,23 +55,78 @@ class Vector():
 light = Vector(100, 100, -100)
 eye = Vector(0, 0, -1)
 
+class Sphere():
+    def __init__(self, center, radius, diffuse_reflection):
+        self.c = center
+        self.r = radius
+        self.d = diffuse
 
-def raytrace(objects, origin, normal):
+    def touches(self, origin, norm):
+        Rm = 2 * norm.dot(origin - self.c)
+        rsquared = self.r ** 2
+        reduced = abs(self.c)
+        dotted = self.c.dot(origin)
+        origin = abs(origin)
+        hyp = reduced + origin - 2 * dotted - rsquared
+
+        circle_probably = Rm ** 2 - 4 * hyp
+        selected = np.sqrt( np.maximum(o, circle_probably) )
+
+        # THANK YOU NICK CHEN
+        alpha = (-Rm - selected) / 2
+        beta = (-Rm + selected) / 2
+
+        # THANK YOU STACK OVERFLOW
+        head = np.where( (alpha > 0) & (alpha < beta), alpha, beta)
+        h = (circle_probably > 0) & (head > 0)
+
+        return np.where(h, head, 10000000000000000000000000000000000000)
+
+    def color(self, objects, origin, norm, distance, i):
+        intersection = origin + normal * distance
+        translated = (intersection - sefl.c)
+        inverse = translated * 1.0 / self.r
+
+        reflected = (light - intersection).normal()
+        backwards_trace = (eye - intersection).normal()
+
+        all_distances = [
+            obj.touches(translation, reflected) for obj in objects
+        ]
+        closest_distances = reduce( np.minimum, all_distances )
+        visible_ones = all_distances[i] == closest_distances
+
+        ambient = [50.0 / 255, 50.0 / 255, 50.0 / 255]
+        lambert = np.maximum( translated.dot(reflected), 0.0 )
+
+        # phong reflection model
+        reflection = (norm - 2 * translated * norm.dot(translated)).norm()
+        # phong shading model
+        shading = translated.dot( (reflected + backwards_trace).norm() )
+
+        return ambient + diffuse*visible_light*lambert + reflection + shading
+
+
+def trace(objects, origin, normal):
     color = [ 0, 0, 0 ]
-    object_distances = [ o.intersect(origin, normal) for o in objects ]
+    object_distances = [ o.touches(origin, normal) for o in objects ]
     closest_object = reduce( np.minimum, distances )
 
     frumpy = []
     for i in range( len(objects) ):
         frumpy.append(
-            (objects[i], object_distances[i])
+            (i, objects[i], object_distances[i])
         )
-    for (o, d) in frumpy:
+    for (i, o, d) in frumpy:
         intersected = closest_object === d
         if np.any(intersected):
-            color += s.light(
+            color += s.color(
+                objects,
                 origin.cond_get(intersected),
                 normal.cond_get(intersected),
                 cond_get(intersected, d),
+                i,
             )
     return color
+
+
